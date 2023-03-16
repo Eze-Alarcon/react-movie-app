029892dd631f564db4ed3e5243df8f50f3b8c3ef
ezequiel <ps3ezequiel@gmail.com>
2023-03-16 20:30:40 +0100

    --ammend

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable comma-dangle */
 /* eslint space-before-function-paren: 0 */
 
-import React from 'react'
+import React, { useState } from 'react'
 // SVG
 import { Mark } from '../../resources/svg/movies/IconsBookmarks'
 import { CategoryMovie } from '../../resources/svg/movies/IconsCategories'
@@ -11,6 +11,7 @@ import { images as config } from '../../storage/config.json'
 // import { results as movie } from '../../mocks/popularMovie.json'
 
 function MovieCard({ movie }) {
+  const [marked, setMarked] = useState(false)
   const { base_url, backdrop_sizes } = config
 
   const mappedMovie = {
@@ -20,17 +21,24 @@ function MovieCard({ movie }) {
     title: movie.title,
   }
 
+  function handleClick() {
+    setMarked((prevState) => !prevState)
+  }
+
   return (
     <>
-      <article className='grid gap-2'>
+      <article className='grid gap-2 hover:cursor-pointer selection:bg-transparent'>
         <div className='w-full aspect-video relative'>
           <img
             src={mappedMovie.imgPath}
             alt=''
             className='object-cover rounded-lg'
           />
-          <div className='grid place-content-center absolute h-8 aspect-square bg-black bg-opacity-60 rounded-full top-2 right-2'>
-            <Mark marked={false} />
+          <div
+            onClick={handleClick}
+            className='grid place-content-center absolute h-8 aspect-square bg-black bg-opacity-60 rounded-full top-2 right-2'
+          >
+            <Mark marked={marked} />
           </div>
         </div>
         <div className='grid gap-1'>
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -13,7 +13,10 @@ import {
 function Header() {
   return (
     <div className='h-14 w-full relative z-50 lg:h-full'>
-      <div id='sticky' className='max-w-full h-14  fixed'>
+      <div
+        id='sticky'
+        className='max-w-full h-14 fixed top-0 pt-6 bg-black lg:top-6 lg:pt-0 lg:left-0 lg:pl-6'
+      >
         <nav className='bg-darkBlue h-14 w-full flex justify-between items-center px-4 md:h-[72px] rounded-xl lg:h-full lg:w-[92px] lg:flex-col lg:py-8 lg:gap-20'>
           <div className='h-7 aspect-square md:h-8'>
             <IconLogo />
